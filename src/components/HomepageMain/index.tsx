f8916a635b9cfccbe6c8f1304edc9791d6ca0c53
Dinko Duric <99042950+dinkoduric@users.noreply.github.com>
2023-07-22 14:29:45 +0200

    Update index.tsx

--- src/components/HomepageMain/index.tsx
+++ src/components/HomepageMain/index.tsx
@@ -93,7 +93,7 @@ function Feature({ title, icon, description, later }: FeatureItem) {
 export default function HomepageFeatures(): JSX.Element {
   const bannerList = [
     {
-      laptop: require('https://static.story.hr/Picture/381103/jpeg/Jadranka-Kosor.jpeg').default,
+      laptop: require('@https://static.story.hr/Picture/381103/jpeg/Jadranka-Kosor.jpeg').default,
       mobile: require('@site/static/img/home-mobile@2x.png').default,
     },
     {
